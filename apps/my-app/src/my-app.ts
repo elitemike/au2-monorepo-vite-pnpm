f4dfd67170782a18ac6a07af445ac79b4b2c8fb7
Mike Stankovich <mike.stankovich@outlook.com>
2024-12-26 14:20:43 -0500

    Updates to build Update to launch for working debugging in vs code

--- apps/my-app/src/my-app.ts
+++ apps/my-app/src/my-app.ts
@@ -1,3 +1,7 @@
 export class MyApp {
   public message = 'Hello World from my app.';
+
+  onMainButtonClick() {
+    console.debug('my app button click')
+  }
 }
--- packages/p1/src/hello-world.ts
+++ packages/p1/src/hello-world.ts
@@ -4,4 +4,8 @@ import template from "./hello-world.html?raw";
 @customElement({ name: "hello-world", template })
 export class HelloWorld {
   @bindable message = "from P1..";
+
+  onButtonClick() {
+    console.debug("on click")
+  }
 }
