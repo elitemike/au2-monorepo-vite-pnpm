9da900d135dc57acc3b08fdd6ea33cdc13bb0bf1
Mike Stankovich <mike.stankovich@outlook.com>
2025-01-13 22:58:39 -0500

    Added ea to main app

--- apps/my-app/src/my-app.ts
+++ apps/my-app/src/my-app.ts
@@ -1,9 +1,18 @@
-import { ILogger, resolve } from 'aurelia';
+
+import { ServiceStartedEvent } from '@exper/p1';
+import { IEventAggregator, ILogger, resolve } from 'aurelia';
 
 export class MyApp {
   private readonly logger: ILogger = resolve(ILogger).scopeTo('MyApp');
+  private readonly ea: IEventAggregator = resolve(IEventAggregator);
 
   public message = 'Hello World from my app.';
+
+  constructor() {
+    this.ea.subscribe(ServiceStartedEvent, (e: ServiceStartedEvent) => {
+      console.debug("event received at main app");
+    });
+  }
 
   onMainButtonClick() {
     console.debug('my app button click')
--- packages/p1/src/index.ts
+++ packages/p1/src/index.ts
@@ -1,3 +1,4 @@
 export { HelloWorld } from './hello-world';
 export { P1Configuration } from "./configuration";
 export { MockService } from "./mockService";
+export { ServiceStartedEvent } from "./serviceStartedEvent";
